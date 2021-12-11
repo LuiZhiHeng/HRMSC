276f42a18cf5c98458cff7a2447d6d6c023b2693
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2021-12-11 12:14:31 +0800

    simplify code

--- template/home_employee.php
+++ template/home_employee.php
@@ -3,163 +3,47 @@
         set_h1("Leave Request"); 
         $uId = get_logged_user_id();
         $today = get_date_now();
+        show_request("Approved", "leave.php", "<i class='fas fa-business-time'></i>", "success", "SELECT count(*) FROM leave_request WHERE leaveStatus=1 AND employeeId='$uId' AND endLeaveDateTime>='$today'");
+        show_request("Pending", "leave.php", "<i class='fas fa-business-time'></i>", "warning", "SELECT count(*) FROM leave_request WHERE leaveStatus=3 AND employeeId='$uId'");
+        show_request("Rejected", "leave.php", "<i class='fas fa-business-time'></i>", "danger", "SELECT count(*) FROM leave_request WHERE leaveStatus=2 AND employeeId='$uId' AND endLeaveDateTime<='$today'");
     ?>
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-success text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Approved Leave Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM leave_request WHERE leaveStatus=1 AND employeeId='$uId' AND endLeaveDateTime>='$today'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-business-time"></i>
-                </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="leave.php">View Request</a>
-                <i class="fas fa-angle-right"></i>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-warning text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Pending Leave Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM leave_request WHERE leaveStatus=3 AND employeeId='$uId'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-business-time"></i>
-                </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="leave.php">View Request</a>
-                <i class="fas fa-angle-right"></i>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-danger text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Rejected Leave Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM leave_request WHERE leaveStatus=2 AND employeeId='$uId' AND endLeaveDateTime<='$today'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-business-time"></i>
-                </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="leave.php">View Request</a>
-                <i class="fas fa-angle-right"></i>
-            </div>
-        </div>
-    </div>
 </div>
-<!-- Claim -->
 <div class="row">
-    <?php set_h1("Claim Request"); ?>
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-success text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Approved Claim Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM claim_request WHERE claimStatus=1 AND employeeId='$uId'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-coins"></i>
+    <?php 
+        set_h1("Claim Request"); 
+        show_request("Approved", "claim.php", "<i class='fas fa-coins'></i>", "success", "SELECT count(*) FROM claim_request WHERE claimStatus=1 AND employeeId='$uId'");
+        show_request("Pending", "claim.php", "<i class='fas fa-coins'></i>", "warning", "SELECT count(*) FROM claim_request WHERE claimStatus=3 AND employeeId='$uId'");
+        show_request("Rejected", "claim.php", "<i class='fas fa-coins'></i>", "danger", "SELECT count(*) FROM claim_request WHERE claimStatus=1 AND employeeId='$uId'");
+    ?>
+</div>
+<?php
+function show_request($head, $link, $icon, $bgColour, $sql){
+    global $conn;
+?>
+<div class="col-4 mb-4">
+    <div class="card bg-<?= $bgColour ?> text-white h-100">
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="me-3">
+                    <div class="text-white-75 small"><?= $head ?></div>
+                    <?php
+                    $rs = $conn->query($sql);
+                    if(!$rs) echo "00";
+                    elseif($rs->num_rows > 0){
+                        $data = $rs->fetch_array(MYSQLI_NUM);
+                        echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
+                    } else echo '0';
+                    ?>
                 </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="claim.php">View Request</a>
-                <i class="fas fa-angle-right"></i>
+                <?= $icon ?>
             </div>
         </div>
-    </div>
-
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-warning text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Pending Claim Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM claim_request WHERE claimStatus=3 AND employeeId='$uId'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-coins"></i>
-                </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="claim.php">View Request</a>
-                <i class="fas fa-angle-right"></i>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-6 col-xl-4 mb-4">
-        <div class="card bg-danger text-white h-100">
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="me-3">
-                        <div class="text-white-75 small">Rejected Claim Request</div>
-                        <?php
-                        $sql = "SELECT count(*) FROM claim_request WHERE claimStatus=2 AND employeeId='$uId'";
-                        $rs = $conn->query($sql);
-                        
-                        if($rs->num_rows > 0){
-                            $data = $rs->fetch_array(MYSQLI_NUM);
-                            echo '<div class="text-lg fw-bold">' . $data[0] . '</div>';
-                        }
-                        ?>
-                    </div>
-                    <i class="fas fa-coins"></i>
-                </div>
-            </div>
-            <div class="card-footer d-flex align-items-center justify-content-between small">
-                <a class="text-white stretched-link" href="claim.php?r=y">View Request</a>
-                <i class="fas fa-angle-right"></i>
-            </div>
+        <div class="card-footer d-flex align-items-center justify-content-between small">
+            <a class="text-white stretched-link" href="<?= $link ?>">Manage Request</a>
+            <i class="fas fa-angle-right"></i>
         </div>
     </div>
 </div>
+<?php
+}
+?>
