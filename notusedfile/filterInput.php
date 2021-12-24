c2235919a155291323a2fc7617de7b93bc61019e
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2021-12-24 18:08:05 +0800

    delete unused file & folder

--- notusedfile/filterInput.php
+++
@@ -1,9 +0,0 @@
-<?php
-function filterInput($input) {
-    $input = trim($input);
-    $input = stripslashes($input);
-    $input = addslashes($input);
-    $input = htmlspecialchars($input);
-	return $input;
-}
-?>
