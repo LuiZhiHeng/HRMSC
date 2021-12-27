2bf1847651ad639efaf99a03c3f1e4e5ff1d3470
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2021-12-27 20:44:58 +0800

    fix update bug (update first only)

--- function/config/editPermission.php
+++ function/config/editPermission.php
@@ -2,13 +2,16 @@
     if(isset($_POST['save'])){
         $rs = $conn->query("SELECT * FROM permission");
         if($rs->num_rows > 0) {
+            $success = 0;
             for ($i=0; $i < $rs->num_rows; $i++) { 
                 $data = $rs->fetch_assoc();
                 $page = $data['name'];
                 $lvl = isset($_POST[$page]) ? 2 : 1;
-                $sql = "UPDATE permission SET permissionLevel = '$lvl' WHERE name = '$page'";
-                swal_result($sql, "Edit Permission", "", "");
+                if($conn->query("UPDATE permission SET permissionLevel = '$lvl' WHERE name = '$page'")) $success = 1;
+                else $success = 0;
             }
+            if($success == 1) swal("Edit Permission Successfully", "", "success");
+            else swal("Edit Permission Failed", "", "error");
         }
     }
 ?>
