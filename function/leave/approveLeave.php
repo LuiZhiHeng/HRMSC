40bed39fd08816ffc05bc11753563d94d8caf257
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2022-01-08 00:53:49 +0800

    fix missing $status & sql(approveDT)

--- function/leave/approveLeave.php
+++ function/leave/approveLeave.php
@@ -2,11 +2,16 @@
     if(isset($_POST['lId'])){
         $lId = filterInput(($_POST['lId']));
 
-        if(isset($_POST['approve'])) $manage = "Approve";
-        elseif(isset($_POST['reject'])) $manage = "Reject";
-        else $manage = "Manage";
+        if(isset($_POST['approve'])) {
+            $manage = "Approve";
+            $status = 1;
+        } elseif(isset($_POST['reject'])){
+            $manage = "Reject";
+            $status = 2;
+        } else $manage = "Manage";
+        $datetimeNow = get_now();
 
-        $sql = "UPDATE leave_request SET leaveStatus='$status' WHERE leaveId='$lId'";
+        $sql = "UPDATE leave_request SET leaveStatus='$status', approveLeaveDateTime='$datetimeNow' WHERE leaveId='$lId'";
         swal_result($sql, $manage . " Leave", "", "");
     }
 ?>
