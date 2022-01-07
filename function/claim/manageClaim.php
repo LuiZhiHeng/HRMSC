479dbfd804421841b80998ae74492d4ce219c2e9
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2022-01-08 01:15:17 +0800

    fix missing $status & sql(approveDT)

--- function/claim/manageClaim.php
+++ function/claim/manageClaim.php
@@ -1,14 +1,21 @@
 <?php
     if(isset($_POST['cid'])){
         
-        if(isset($_POST['approve'])) $manage = "Approve";
-        elseif(isset($_POST['reject'])) $manage = "Reject";
-        elseif(isset($_POST['prepare'])) $manage = "Perpare";
-        else $manage = "Manage";
+        if(isset($_POST['approve'])){
+            $manage = "Approve";
+            $status = 1;
+        } elseif(isset($_POST['reject'])){
+            $manage = "Reject";
+            $status = 2;
+        } elseif(isset($_POST['prepare'])){
+            $manage = "Perpare";
+            $status = 5;
+        } else $manage = "Manage";
         
+        $datetimeNow = get_now();
         $lId = filterInput(($_POST['cid']));
 
-        $sql = "UPDATE claim_request SET claimStatus='$status' WHERE claimId = '$lId'";
+        $sql = "UPDATE claim_request SET claimStatus='$status', approveClaimDateTime='$datetimeNow' WHERE claimId = '$lId'";
         swal_result($sql, $manage . " Claim", "", "");
     }
 ?>
