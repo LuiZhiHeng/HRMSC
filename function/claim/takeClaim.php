541736955dfd619f08ffb0ee77adc548caee5a24
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2022-01-08 01:13:53 +0800

    fix mistype

--- function/claim/takeClaim.php
+++ function/claim/takeClaim.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_POST['id']) && isset($_POST['take'])){
-        $lId = filterInput(($_POST[' id']));
+        $lId = filterInput(($_POST['id']));
 
         $sql = "UPDATE claim_request SET claimStatus = 6 WHERE claimId='$lId'";
         swal_result($sql, "Take Claim", "", "");
