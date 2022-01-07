ea6468cfa05549830d12121d72b599d536de3e45
lui <46946117+LuiZhiHeng@users.noreply.github.com>
2022-01-08 01:17:54 +0800

    fix mistype

--- function/payroll/generatePayroll.php
+++ function/payroll/generatePayroll.php
@@ -6,7 +6,7 @@
         //check is existed?
         $rsCheck = $conn->query("SELECT month, year FROM payroll where month = '$monthSelected' AND year = '$yearSelected'");
         if(!$rsCheck) console("Fail to check payroll");
-        elseif($rsCheck->num_rows > 0) swal("Generate Payroll failed!", "Payroll for ' . $monthSelected . "/" . $yearSelected . ' is already generated before", "warning");
+        elseif($rsCheck->num_rows > 0) swal("Generate Payroll failed!", "Payroll for '" . $monthSelected . "/" . $yearSelected . "' is already generated before", "warning");
         elseif($rsCheck->num_rows == 0){// data not exist == generate payroll
             $status = create_payroll($monthSelected, $yearSelected, 0);
             if($status == 0) swal("Generate Payroll Failed!", "Fail to generate payroll for \'" . $monthSelected . "/" . $yearSelected . "\'", "error");
